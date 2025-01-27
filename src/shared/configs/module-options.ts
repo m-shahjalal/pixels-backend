00ebc58b57c1807c224a95ef1c11f7a4d798c606
shahjalal <lx.shahjalal@gmail.com>
2025-01-27 22:42:18 +0600

    - Type can be: feat, fix, docs, style, refactor, test, or chore - Scope should be one of: api, auth, db, ui, docs, or test - Description should be under 50 characters and use imperative mood - Body can include bullet points for detailed changes - Reference any related issues in the footer

--- src/shared/configs/module-options.ts
+++ src/shared/configs/module-options.ts
@@ -1,7 +1,21 @@
 import { ConfigModuleOptions } from '@nestjs/config/dist/interfaces';
-import { validationSchema } from './env-schema';
-
+import { z } from 'zod';
 import configuration from './configuration';
+
+export const validationSchema = z.object({
+  APP_ENV: z.enum(['development', 'production', 'test']).default('development'),
+  APP_PORT: z.coerce.number().min(2000).max(65_535),
+  DB_HOST: z.string().min(1),
+  DB_PORT: z.coerce.number().min(2000).max(65_535).default(5432),
+  DB_NAME: z.string().min(3),
+  DB_USER: z.string().min(3),
+  DB_PASS: z.string().min(3),
+  JWT_PUBLIC_KEY_BASE64: z.string().min(1),
+  JWT_PRIVATE_KEY_BASE64: z.string().min(1),
+  JWT_ACCESS_TOKEN_EXP_IN_SEC: z.coerce.number().positive(),
+  JWT_REFRESH_TOKEN_EXP_IN_SEC: z.coerce.number().positive(),
+  DEFAULT_ADMIN_USER_PASSWORD: z.string().min(6),
+});
 
 export const configModuleOptions: ConfigModuleOptions = {
   envFilePath: '.env',
