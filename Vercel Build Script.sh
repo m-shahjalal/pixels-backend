38796b1d46e3b2118b4e47bf0b3c5d6a7cd27aae
shahjalal <lx.shahjalal@gmail.com>
2025-04-06 10:55:27 +0600

    feat: Streamline build and deployment process
    - Updated build script in package.json to copy necessary files to dist directory and added vercel-build script for deployment automation.
    - Added module-alias dependency to enhance module resolution.
    - Changed Vercel configuration to use dist/main.js for deployment.
    - Removed compiler options from tsconfig.build.json, simplifying the configuration but potentially impacting module resolution.
    
    This commit builds on previous efforts to improve the build process and module imports.

---
+++ Vercel Build Script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+npm run build
+cp register-paths.js dist/
+cp tsconfig.json dist/
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import '../register-paths';
+
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
