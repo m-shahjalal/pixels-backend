32b42e46f5e8e54b919e6e760e6435f75c79fa54
shahjalal <lx.shahjalal@gmail.com>
2025-04-06 11:53:38 +0600

    feat: Enhance application initialization and add build check script
    - Added `build:check` script in package.json for type checking without emitting files.
    - Refactored `bootstrap` function in main.ts to prevent reinitialization of the app.
    - Introduced Vercel handler function for serverless deployment and added graceful shutdown logic.

--- src/main.ts
+++ src/main.ts
@@ -5,31 +5,60 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CONFIG_KEY } from './config/config';
 import { customOptions, swaggerConfig } from './config/swagger.config';
+import { Express, Request, Response } from 'express';
+
+let app: any;
+let expressApp: Express;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = app.get(ConfigService).get(CONFIG_KEY);
-  const { port, apiPrefix } = config.app;
+  if (!app) {
+    app = await NestFactory.create(AppModule);
+    const config = app.get(ConfigService).get(CONFIG_KEY);
+    const { apiPrefix } = config.app;
+    app.enableCors();
 
-  app.enableCors();
+    app.setGlobalPrefix(apiPrefix, {
+      exclude: ['/ping', '/health', '/docs', '/'],
+    });
 
-  app.setGlobalPrefix(apiPrefix, {
-    exclude: ['/ping', '/health', '/docs', '/'],
-  });
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+      }),
+    );
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-      transform: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+    const document = SwaggerModule.createDocument(app, swaggerConfig);
+    SwaggerModule.setup('docs', app, document, customOptions);
 
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('docs', app, document, customOptions);
-
-  await app.listen(port);
-  console.info(`🚀🚀 http://localhost:${port}`);
+    await app.init();
+    expressApp = app.getHttpAdapter().getInstance();
+    console.info(`🚀🚀 Application initialized successfully`);
+  }
+  return expressApp;
 }
 
-bootstrap();
+// This is the handler function for Vercel
+export default async function handler(req: Request, res: Response) {
+  const app = await bootstrap();
+  return app(req, res);
+}
+
+// If running in non-serverless environment (like local development)
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then((app) => {
+    const server = app.listen(process.env.PORT || 4000, () => {
+      console.info(
+        `🚀🚀 Server running on http://localhost:${process.env.PORT || 4000}`,
+      );
+    });
+
+    // Handle shutdown gracefully
+    process.on('SIGTERM', () => {
+      server.close(() => {
+        console.info('Server closed');
+      });
+    });
+  });
+}
